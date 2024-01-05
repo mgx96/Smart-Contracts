bc3c749142d5453690a2b7c727e53a03fec93b62
Malek Sharabi <m.malek1996@hotmail.com>
2024-01-05 23:21:38 +0100

    Store & Retrieve Contract (SOL)

--- Simple Contracts/Store & Retrieve Contract/SimpleStorage.sol
+++ Simple Contracts/Store & Retrieve Contract/SimpleStorage.sol
@@ -4,11 +4,22 @@ pragma solidity ^0.8.18;
 contract SimpleStorage{
    uint256 testNumber;
 
+   struct Person{
+    uint256 favoriteNumber;
+    string name;
+   }
+
+   Person[] public listOfPeople;
+
    function store(uint256 _testNumber) public {
     testNumber = _testNumber;
    }
 
    function retrieve() public view returns(uint256){
     return testNumber;
+   }
+
+   function addPerson(string memory _name, uint256 _favoriteNumber) public {
+    listOfPeople.push(Person(_favoriteNumber,_name));
    }
 }
