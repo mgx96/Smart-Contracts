68f4047819f8233dbc3989bfc834eefdd56b615d
Malek Sharabi <m.malek1996@hotmail.com>
2024-01-22 20:06:06 +0100

    Contract Improvements

--- Simple Contracts/Contract Deploying Other Contracts/StorageFactory.sol
+++ Simple Contracts/Contract Deploying Other Contracts/StorageFactory.sol
@@ -1,14 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "./BasicContract.sol";
+import {BasicContract} from "./BasicContract.sol";
 
 contract StorageFactory{
 
-    BasicContract public basicContract;
+    BasicContract[] public listOfBasicContracts;
 
     function createBasicContract () public{
 
-        basicContract = new BasicContract();
+        BasicContract newListOfBasicContracts = new BasicContract();
+        listOfBasicContracts.push(newListOfBasicContracts);
+    }
+
+    function sfStore (uint256 _basicContractIndex, uint256 _newBasicContractNumber) public {
+        BasicContract myBasicContract = listOfBasicContracts[_basicContractIndex];
+        myBasicContract.store(_newBasicContractNumber);
+    }
+
+    function sfGet (uint256 _basicContractIndex) public view returns(uint256){
+        BasicContract myBasicContract = listOfBasicContracts[_basicContractIndex];
+        return myBasicContract.retrieve();
     }
 }
