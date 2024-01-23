5ca7b4066bec7818eb760ef96bad01ca1979910b
Malek Sharabi <m.malek1996@hotmail.com>
2024-01-23 21:42:22 +0100

    Added A Transaction Limit

--- Simple Contracts/Fundraiser Contract Using Chainlink Price Oracles/Fundraising.sol
+++ Simple Contracts/Fundraiser Contract Using Chainlink Price Oracles/Fundraising.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract Fundraising{
 
     uint256 minimumUsd = 5;
@@ -10,8 +12,16 @@ contract Fundraising{
         require(msg.value >= minimumUsd, "The minimum required amount is 1 ETH");
     }
     
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // Address : 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,)= priceFeed.latestRoundData();
+        //Matching the number of 0's with msg.value 
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate() public {
+
     }
 
     function withdraw() public {
