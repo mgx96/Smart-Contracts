19396b34299c2a236244679ba15020cce1e6129c
Malek Sharabi <m.malek1996@hotmail.com>
2024-01-28 15:47:41 +0100

    Reset wallet balance upon withdrawal

--- Simple Contracts/Fundraiser Using Libraries/Fundraising_Library.sol
+++ Simple Contracts/Fundraiser Using Libraries/Fundraising_Library.sol
@@ -23,7 +23,11 @@ contract Fundraising{
     function withdraw() public {
         for (uint256 listOfUsersIndex = 0; listOfUsersIndex < listOfUsers.length; listOfUsersIndex++){
             address user = listOfUsers[listOfUsersIndex];
+            //resetting the wallet's balance to 0
             addressToAmountFunded[user] = 0;
         }
+        listOfUsers = new address[](0);
+        (bool callSuccess,) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed!");
     }
 }
